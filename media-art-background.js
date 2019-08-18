3091938077dfb3db64d088703951c852e6aa72b4
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-08-19 00:22:39 +0200

    add parameter to blur background image
    example config:
    
    media_art_background:
      blur: 10px
      entities:
        - entity: media_player.spotify
          valid_states:
            - playing
          image_attribute: entity_picture

--- media-art-background.js
+++ media-art-background.js
@@ -1,9 +1,10 @@
-function setBackground(root, lovelace, app, originalStyle) {
+function setBackground(root, lovelace, bground_elem, originalStyle) {
   let hass = root.hass;
   for (let entity of lovelace.config.media_art_background.entities) {
     let entityName = entity.entity || entity;
     let entityValidStates = entity.valid_states || ['playing'];
     let entityImageAttribute = entity.image_attribute || 'entity_picture';
+    let filterBlur = lovelace.config.media_art_background.blur || '0px';
 
     let entityInfo = hass.states[entity];
 
@@ -17,23 +18,37 @@ function setBackground(root, lovelace, app, originalStyle) {
     const backgroundUrl = entityInfo.attributes[entityImageAttribute];
     if (!backgroundUrl) continue;
 
-    app.style.backgroundImage = `url('${backgroundUrl}')`
-    app.style.backgroundRepeat = 'no-repeat';
-    app.style.backgroundPosition = 'center';
-    app.style.backgroundSize = 'cover';
-    app.style.backgroundAttachment = 'fixed';
+    bground_elem.style.backgroundImage = `url('${backgroundUrl}')`
+    bground_elem.style.backgroundRepeat = 'no-repeat';
+    bground_elem.style.backgroundPosition = 'center';
+    bground_elem.style.backgroundSize = 'cover';
+    bground_elem.style.backgroundAttachment = 'fixed';
+    bground_elem.style.filter = `blur(${filterBlur})`;
 
     return;
   }
 
-  app.style = originalStyle;
+  bground_elem.style = originalStyle;
 };
 
 let root = document.querySelector("home-assistant");
 root = root.shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("app-drawer-layout partial-panel-resolver ha-panel-lovelace").shadowRoot.querySelector("hui-root");
 const lovelace = root.lovelace;
 const app = root.shadowRoot.querySelector("ha-app-layout");
-const originalStyle = app.style;
 
-setInterval(function() { setBackground(root, lovelace, app, originalStyle) }, 5000);
-setBackground(root, lovelace, app, originalStyle);
+const bground = document.createElement("div"); // create empty container for background 
+
+bground.style.position = "absolute"; // fill entire window
+bground.style.top = 0;
+bground.style.left = 0;
+bground.style.width = "100%";
+bground.style.height = "100%";
+
+bground.style.zIndex = -1; // below view elements
+
+app.appendChild(bground);
+
+const originalStyle = bground.style;
+
+setInterval(function() { setBackground(root, lovelace, bground, originalStyle) }, 5000);
+setBackground(root, lovelace, bground, originalStyle);
